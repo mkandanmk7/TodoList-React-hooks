0cada0c71e50d74bd6ef7267b8c5919e23025017
muthu <mkandanmk7@gmail.com>
2021-09-08 16:45:26 +0530

    added todolist

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import TodoList from "./compnents/TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To do List</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
